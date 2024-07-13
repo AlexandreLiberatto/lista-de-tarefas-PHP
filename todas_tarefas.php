7b4fee31f1426b7c762ccddc3acdc02d882ea134
unknown <alexandreliberatto@gmail.com>
2024-07-13 18:56:45 -0300

    Listando tarefas no HTML

--- todas_tarefas.php
+++ todas_tarefas.php
@@ -14,6 +14,32 @@ require 'tarefa_controller.php';
 		<link rel="stylesheet" href="css/estilo.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+	<script>
+		function editar() {
+			
+			let form = document.createElement('form')
+			form.action = '#'
+			form.method = 'post'
+
+			let inputTarefa = document.createElement('input')
+			inputTarefa.type = 'text'
+			inputTarefa.name = 'tarefa'
+			inputTarefa.className = 'form-control'
+
+			let button = document.createElement('button')
+			button.type = 'submit'
+			button.className = 'btn btn-info'
+			button.innerHTML = 'Atualizar'
+
+			form.appendChild(inputTarefa)
+
+			form.appendChild(button)
+
+			//teste
+			console.log(form)
+		}
+	</script>
+	
 	</head>
 
 	<body>
@@ -43,23 +69,17 @@ require 'tarefa_controller.php';
 								<h4>Todas tarefas</h4>
 								<hr />
 
-								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9">Lavar o carro (status)</div>
-									<div class="col-sm-3 mt-2 d-flex justify-content-between">
-										<i class="fas fa-trash-alt fa-lg text-danger"></i>
-										<i class="fas fa-edit fa-lg text-info"></i>
-										<i class="fas fa-check-square fa-lg text-success"></i>
+								<?php foreach($tarefas as $indice => $tarefa) { ?>
+									<div class="row mb-3 d-flex align-items-center tarefa">
+										<div class="col-sm-9"><?= $tarefa->tarefa ?> (<?= $tarefa->status ?>)</div>
+										<div class="col-sm-3 mt-2 d-flex justify-content-between">
+											<i class="fas fa-trash-alt fa-lg text-danger"></i>
+											<i class="fas fa-edit fa-lg text-info" onclick="editar()"></i>
+											<i class="fas fa-check-square fa-lg text-success"></i>
+										</div>
 									</div>
-								</div>
 
-								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9">Passear com o cachorro (status)</div>
-									<div class="col-sm-3 mt-2 d-flex justify-content-between">
-										<i class="fas fa-trash-alt fa-lg text-danger"></i>
-										<i class="fas fa-edit fa-lg text-info"></i>
-										<i class="fas fa-check-square fa-lg text-success"></i>
-									</div>
-								</div>
+								<?php } ?>
 								
 							</div>
 						</div>
